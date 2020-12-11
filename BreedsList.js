d7b8d2bd49a9a8084af0f86f479fc9878127d596
Leandro Fernandez <leandroofernandezz@gmail.com>
2020-12-11 14:56:25 -0300

    remove try

--- BreedsList.js
+++ BreedsList.js
@@ -6,18 +6,14 @@ function BreedsList({handleBreedClick}) {
 
   async function fetchBreeds() {
     if(!loading) return
-    try {
-      const response = await new Promise(solve => {
-        setTimeout(() => {
-          solve(fetch('https://dog.ceo/api/breeds/list/all'))
-        }, 1000)
-      })
-      const responseJson = await response.json()
-      setBreeds(Object.keys(responseJson.message))
-      setLoading(false)
-    } catch (e) {
-      throw(e)
-    }
+    const response = await new Promise(solve => {
+      setTimeout(() => {
+        solve(fetch('https://dog.ceo/api/breeds/list/all'))
+      }, 1000)
+    })
+    const responseJson = await response.json()
+    setBreeds(Object.keys(responseJson.message))
+    setLoading(false)
   }
   
 
